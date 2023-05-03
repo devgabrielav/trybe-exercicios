ff1bde0189c86219ab190e3f815ad319838f6079
devgabrielav <gabrivicentini@icloud.com>
2023-05-03 20:59:45 -0300

    Tentativa pela metade exercicios dia 2 seção 3

---
+++ fundamentos/secao-3/dia-2/scripts.js
@@ -0,0 +1,25 @@
+/* 
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+for (index = 0; index < groceryList.length; index += 1) {
+    console.log(groceryList[index]) 
+}
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+for (let name of names){
+console.log(name);
+}*/
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sum = 0;
+
+for (index = 0; index < numbers.length; index += 1) {
+    console.log(numbers[index]);
+    sum += numbers[index];
+    console.log(sum);
+    
+
+}
+
+
