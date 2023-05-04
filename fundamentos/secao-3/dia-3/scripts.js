74912e4b1e3ca3a8dfdc00ef91434d1f8c2c94e5
devgabrielav <gabrivicentini@icloud.com>
2023-05-04 18:27:15 -0300

    Atividade 2

--- fundamentos/secao-3/dia-3/scripts.js
+++ fundamentos/secao-3/dia-3/scripts.js
@@ -1,4 +1,4 @@
-let saldo = 2000;
+/* let saldo = 2000;
 
 
 function adicionaValor(adicao) {
@@ -17,4 +17,30 @@ function diviValor(divi) {
 console.log(adicionaValor(500));
 console.log(subtracaoValor(250));
 console.log(multValor(5));
-console.log(diviValor(2));
+console.log(diviValor(2)); */
+
+let clientesTrybeBank = ['Ada', 'John', 'Gus'];
+
+function addClient(nome) {
+    if (typeof nome === 'string'){
+        clientesTrybeBank.push(nome);
+        return 'Cliente adicionado com sucesso!'
+    } else {
+        return 'Parâmetro não é uma string!'
+    }
+}
+function removeClient(nome) {
+    if (typeof nome === 'string'){
+        clientesTrybeBank.pop(nome);
+        return 'Cliente removido com sucesso!'
+    } else {
+        return 'Parâmetro não é uma string!'
+    }
+}
+
+
+console.log(addClient('Davi'));
+console.log(clientesTrybeBank);
+
+console.log(removeClient('Davi'));
+console.log(clientesTrybeBank);
