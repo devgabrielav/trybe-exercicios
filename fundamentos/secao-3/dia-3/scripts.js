85a90350c8abc50fd33c902ff85c2f1287c284db
devgabrielav <gabrivicentini@icloud.com>
2023-05-04 18:44:58 -0300

    Atividade 3 tentativa 1

--- fundamentos/secao-3/dia-3/scripts.js
+++ fundamentos/secao-3/dia-3/scripts.js
@@ -18,7 +18,7 @@ console.log(adicionaValor(500));
 console.log(subtracaoValor(250));
 console.log(multValor(5));
 console.log(diviValor(2)); */
-
+/* 
 let clientesTrybeBank = ['Ada', 'John', 'Gus'];
 
 function addClient(nome) {
@@ -43,4 +43,37 @@ console.log(addClient('Davi'));
 console.log(clientesTrybeBank);
 
 console.log(removeClient('Davi'));
+console.log(clientesTrybeBank); */
+
+let clientesTrybeBank = ['Ada', 'John', 'Gus'];
+
+function checkCliente(cliente) {
+    let clienteEncontrado;
+    if (typeof cliente === 'string') {
+        clienteEncontrado = clientesTrybeBank;
+        if (clienteEncontrado === false) {
+            return 'Cliente não encontrado'
+        }
+        else {
+            return 'O parâmetro passado deve ser do tipo "string"!';
+        }
+
+    }
+}
+
+console.log(checkCliente('John'));
+
+function removeCliente(cliente) {
+
+    for (let index = 0; index < clientesTrybeBank.length; index += 1) {
+        if (cliente === clientesTrybeBank[index]) {
+            clientesTrybeBank.splice(index, 1);
+            clienteEncontrado = true;
+            return 'Cliente excluída(o) com sucesso.';
+        }
+
+    }
+};
+
+console.log(removeCliente);
 console.log(clientesTrybeBank);
