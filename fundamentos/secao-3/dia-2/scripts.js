1492efa01812d5ca0e5cced91e4994277013cf24
devgabrielav <gabrivicentini@icloud.com>
2023-05-04 21:22:57 -0300

    So falta mais um pouco

--- fundamentos/secao-3/dia-2/scripts.js
+++ fundamentos/secao-3/dia-2/scripts.js
@@ -12,14 +12,54 @@ console.log(name);
 }*/
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let sum = 0;
+/* let sum = 0;
 
 for (index = 0; index < numbers.length; index += 1) {
-    console.log(numbers[index]);
     sum += numbers[index];
-    console.log(sum);
-    
+    //console.log(sum);
+    //console.log(numbers[index]);
+}
 
+let media = numbers.length / 10;
+//console.log(media);
+
+if (media > 20){
+    //console.log('O valor da média aritmética é maior que 20');
+} else {
+    //console.log('O valor da média aritmética é menor ou igual a 20');
 }
 
 
+let numeroMaior = numbers[0];
+
+for (let index = 1; index < numbers.length; index += 1){
+    if (numbers[index] > numeroMaior){
+        numeroMaior = numbers[index];
+        //console.log(numeroMaior);
+    }
+} 
+let numerosImpares;
+for (let index = 1; index < numbers.length; index += 1){
+    if (numbers[index] % 2 != 0){
+        numerosImpares = numbers[index];
+        console.log(numerosImpares);
+    } else {
+        console.log('Nenhum valor ímpar encontrado');
+    }
+} 
+
+
+let numeroMenor = numbers[0];
+
+for (let index = 1; index < numbers.length; index += 1){
+    if ( numbers[index] < numeroMenor){
+        numeroMenor = numbers[index];
+        console.log(numeroMenor);
+    }
+} 
+
+for (let array = 1; array <= 25; array += 1) {
+     console.log(array);
+    let divisao = array / 2;
+    console.log(divisao);
+} */
