4556edfda13ad79777379e48d4b0259156861994
devgabrielav <gabrivicentini@icloud.com>
2023-05-11 16:09:32 -0300

    Inicio seção 4!

---
+++ fundamentos/secao-4/dia-01/scripts.js
@@ -0,0 +1,10 @@
+const paragraph = document.getElementById("paragraph");
+paragraph.style.color = "red";
+document.getElementById('page-title').innerText = 'Avatar';
+const paragraph2 = document.getElementById('second-paragraph');
+paragraph2.style.backgroundColor = 'salmon';
+paragraph2.style.color = 'blue';
+document.getElementById('subtitle').style.color = 'pink';
+
+document.getElementsByClassName('testeP')[0].style.backgroundColor = 'pink';
+document.getElementsByTagName('h4')[0].style.color = 'blue';
