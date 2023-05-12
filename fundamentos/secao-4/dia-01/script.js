92f11c99d95f0da367460a343c56c3c0dce47659
devgabrielav <gabrivicentini@icloud.com>
2023-05-12 20:34:00 -0300

    Aeee foi!

--- fundamentos/secao-4/dia-01/script.js
+++ fundamentos/secao-4/dia-01/script.js
@@ -1,1 +1,25 @@
-document.getElementsByTagName('h1').style = 'black';
+const title = document.querySelector('#header-container');
+title.style.backgroundColor = 'rgb(0, 176, 105)';
+
+const back = document.querySelectorAll('.emergency-tasks h3');
+for (let index = 0; index < back.length; index += 1){
+    back[index].style.backgroundColor = 'rgb(165, 0, 243)';
+}
+
+const back2 = document.querySelectorAll('.no-emergency-tasks h3');
+for (let index = 0; index < back2.length; index += 1){
+    back2[index].style.backgroundColor = 'rgb(35, 37, 37)';
+}
+
+const dad = document.querySelectorAll('.emergency-tasks div'); 
+for (let index = 0; index < dad.length; index += 1){
+    dad[index].style.backgroundColor = 'rgb(255, 159, 132)';
+}
+
+const dad2 = document.querySelectorAll('.no-emergency-tasks div');
+for (let index = 0; index < dad2.length; index += 1){
+    dad2[index].style.backgroundColor = 'rgb(249, 219, 94)';
+}
+
+
+
