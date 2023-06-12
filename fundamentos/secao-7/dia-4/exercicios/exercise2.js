a88bb3d68a461c7eab249db7fcf6dbfc640a9e80
devgabrielav <gabrivicentini@icloud.com>
2023-06-12 19:56:53 -0300

    Exercicios do dia

---
+++ fundamentos/secao-7/dia-4/exercicios/exercise2.js
@@ -0,0 +1,3 @@
+const sum = (...num) => num.reduce((sum, value) => sum + value);
+
+console.log(sum(4,5,6));
---
+++ fundamentos/secao-7/dia-4/exercicios/exercise6.js
@@ -0,0 +1,16 @@
+
+const yearSeasons = {
+  spring: ['March', 'April', 'May'],
+  summer: ['June', 'July', 'August'],
+  autumn: ['September', 'October', 'November'],
+  winter: ['December', 'January', 'February'],
+};
+
+const {spring} = yearSeasons;
+const {summer} = yearSeasons;
+const {autumn} = yearSeasons;
+const {winter} = yearSeasons;
+
+const months = [...spring, ...summer, ...autumn, ...winter];
+
+console.log(months);
