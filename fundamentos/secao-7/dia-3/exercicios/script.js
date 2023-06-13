bb9534784a1e2bd397b6d76f304019fe7f990fea
devgabrielav <gabrivicentini@icloud.com>
2023-06-13 15:22:21 -0300

    Exercicios

---
+++ fundamentos/secao-7/dia-3/exercicios/script.js
@@ -0,0 +1,88 @@
+// Fonte: <https://restcountries.com/v2/all>
+const countries = [
+  {
+    name: 'Afghanistan',
+    region: 'Asia',
+    currencies: [{ code: 'AFN', name: 'Afghan afghani' }],
+    capital: 'Kabul',
+    population: 40218234,
+    area: 652230
+  },
+  {
+    name: 'Åland Islands',
+    region: 'Europe',
+    currencies: [{ code: 'EUR', name: 'Euro' }],
+    capital: 'Mariehamn',
+    population: 28875,
+    area: 1580
+  },
+  {
+    name: 'Albania',
+    region: 'Europe',
+    currencies: [{ code: 'ALL', name: 'Albanian lek' }],
+    capital: 'Tirana',
+    population: 2837743,
+    area: 28748
+  },
+  {
+    name: 'Algeria',
+    region: 'Africa',
+    currencies: [{ code: 'DZD', name: 'Algerian dinar' }],
+    capital: 'Algiers',
+    population: 44700000,
+    area: 2381741
+  },
+  {
+    name: 'American Samoa',
+    region: 'Oceania',
+    currencies: [{ code: 'USD', name: 'United States Dollar' }],
+    capital: 'Pago Pago',
+    population: 55197,
+    area: 199
+  },
+  {
+    name: 'Andorra',
+    region: 'Europe',
+    currencies: [{ code: 'EUR', name: 'Euro' }],
+    capital: 'Andorra la Vella',
+    population: 77265,
+    area: 468
+  },
+  {
+    name: 'Angola',
+    region: 'Africa',
+    currencies: [{ code: 'AOA', name: 'Angolan kwanza' }],
+    capital: 'Luanda',
+    population: 32866268,
+    area: 1246700
+  },
+  {
+    name: 'Anguilla',
+    region: 'Americas',
+    currencies: [{ code: 'XCD', name: 'East Caribbean dollar' }],
+    capital: 'The Valley',
+    population: 13452,
+    area: 91
+  }
+];
+
+const popula = countries.map((country) => country.population).reduce((sum, value) => sum + value);
+const are = countries.map((country) => country.area).reduce((sum, value) => sum + value)
+const longestName = (array) => array.reduce((country, country2) => country2.name.length > country.name.length? country2 : country);
+
+/* console.log(popula);
+console.log(are);
+console.log(longestName(countries)); */
+
+const names = [
+  'Aanemarie', 'Adervandes', 'Akifusa',
+  'Abegildo', 'Adicellia', 'Aladonata',
+  'Abeladerco', 'Adieidy', 'Alarucha',
+];
+
+let soma = 0;
+const countA = (array) => {
+  const letraa = array.split('').forEach((letter) => letter.toUpperCase === 'a');
+  return letraa;
+}
+console.log(countA(names));
