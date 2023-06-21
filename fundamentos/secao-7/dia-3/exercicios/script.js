84e9137cdc3662d4602bef10cf4bb79eaa28cff5
devgabrielav <gabrivicentini@icloud.com>
2023-06-21 15:27:05 -0300

    Fim do módulo ;)

--- fundamentos/secao-7/dia-3/exercicios/script.js
+++ fundamentos/secao-7/dia-3/exercicios/script.js
@@ -80,9 +80,114 @@ const names = [
   'Abeladerco', 'Adieidy', 'Alarucha',
 ];
 
-let soma = 0;
-const countA = (array) => {
-  const letraa = array.split('').forEach((letter) => letter.toUpperCase === 'a');
-  return letraa;
+
+// Exercicio 4
+
+const fullArray = names.join('').toLowerCase().split('');
+
+const countA = (array) => 
+  array.reduce((acc, curr) => {
+    return curr === 'a'? acc += 1 : acc;
+  }, 0);
+
+
+// Exercicio 5
+
+// O index 0 do array `students` equivale ao index 0 do array `grades`
+const students = ['Pedro Henrique', 'Miguel', 'Maria Clara'];
+const grades = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
+const studentAverage = () => {
+  return students.map((student, index) => (
+    {
+    name: student,
+    average: (grades[index].reduce((acc, curr) => acc + curr, 0) / grades[index].length),
+  }
+  ));
 }
-console.log(countA(names));
+
+const arrays = [
+  ['1', '2', '3'],
+  [true],
+  [4, 5, 6],
+];
+
+const flatten = () => {
+  return arrays.reduce((acc, curr) => {
+    return acc + curr
+  })
+}
+
+const books = [
+  {
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: {
+      name: 'George R. R. Martin',
+      birthYear: 1948,
+    },
+    releaseYear: 1991,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: {
+      name: 'J. R. R. Tolkien',
+      birthYear: 1892,
+    },
+    releaseYear: 1954,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Isaac Asimov',
+      birthYear: 1920,
+    },
+    releaseYear: 1951,
+  },
+  {
+    id: 4,
+    name: 'Duna',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Frank Herbert',
+      birthYear: 1920,
+    },
+    releaseYear: 1965,
+  },
+  {
+    id: 5,
+    name: 'A Coisa',
+    genre: 'Terror',
+    author: {
+      name: 'Stephen King',
+      birthYear: 1947,
+    },
+    releaseYear: 1986,
+  },
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: {
+      name: 'H. P. Lovecraft',
+      birthYear: 1890,
+    },
+    releaseYear: 1928,
+  },
+];
+
+const expectedResult = "George R. R. Martin, J. R. R. Tolkien, Isaac Asimov, Frank Herbert, Stephen King, H. P. Lovecraft.";
+const authors = books.map((book) => book.author.name);
+console.log(authors);
+
+/* const reduceNames = () => {
+  return books.reduce((acc, curr) => {
+    return curr === author.name? acc + curr : acc;
+  })
+}
+ */
+/* console.log(reduceNames()); */
